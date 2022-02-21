bd4f249b0c1fc8bada36db5efa5baeb2776e6a51
Zybartas Ringys <developerzr92@gmail.com>
2022-02-21 22:28:41 +0200

    js

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ fetch("data.json")
         createRegularCard(element, timeframe)
       );
     });
+
+    jsonData.forEach((element) => {
+      data[element.title] = element.timeframes;
+    });
+
+    console.log(data);
+
+    cards = document.querySelectorAll(".play-card");
+
+    console.log(cards);
   });
 
 timeOption.forEach((element) => {
@@ -23,12 +33,24 @@ timeOption.forEach((element) => {
 
 // functions
 
-function optionsOnClick(e) {
+function optionsOnClick(event) {
   timeOption.forEach((element) => {
     element.classList.remove("active");
   });
-  e.target.classList.add("active");
-  timeframe = e.target.innerText.toLowerCase();
+  event.target.classList.add("active");
+  timeframe = event.target.innerText.toLowerCase();
+}
+
+// update cards
+
+function updateCards() {
+  cards.forEach((card) => {
+    updateCards(card, timeframe);
+  });
+}
+
+function updateCard() {
+  console.log("update");
 }
 
 function createRegularCard(element, timeframe) {
@@ -40,7 +62,7 @@ function createRegularCard(element, timeframe) {
 
   <div class="col">
                 <div class="card play-card">
-                  <div class="card-img-top play-card-top">
+                  <div class="card-img-top cards-top">
                     <img
                       src="/images/icon-play.svg"
                       alt="Play"
@@ -48,12 +70,19 @@ function createRegularCard(element, timeframe) {
                     />
                   </div>
                   <div class="card-body">
-                    <h5 class="card-title">${title}</h5>
-                    <p class="card-text">
-                      ${current}
-                    </p>
-                    <p>${previous} </p>
+                  <div class="d-flex flex-row justify-content-between"><h5 class="card-title">${title}</h5>
+                  <img
+                  src="/images/icon-ellipsis.svg"
+                  alt="Play"
+                  class="dots"
+                /> </div>
+                    
+                    <div class="card-text">
+                    <h5 class="current">${current}</h5>
+                    <h5 class="last">Last week - ${previous}</h5>
+                    </div> 
                   </div>
+                  
                 </div>
               </div>
   `;
